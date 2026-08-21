fdca4b61c85782776bad732ecc85b68c81bdc55e
Ankesh Kumar <211618357+Ankeshhkumar@users.noreply.github.com>
2026-08-21 22:52:13 +0530

    Prometheus is added now

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.chat import router as chat_router
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI()
 
@@ -12,7 +14,10 @@ allow_methods=["*"],
 allow_headers=["*"],
 )
 
+
 app.include_router(chat_router)
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def home():
